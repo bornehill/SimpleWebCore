e751c160bac73a9b0d52510805814cfd016dbc25
bornehill <ali.escamilla.navarro@gmail.com>
2018-02-24 20:50:12 -0600

    First component

---
+++ SimpleWebCore/ClientApp/app/shop/productList.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "product-list",
+    templateUrl: "productList.component.html",
+    styleUrls: []
+})
+
+export class ProductList {
+    public products = [{
+        title: "First product",
+        price: 18.90
+    },
+    {
+        title: "Second product",
+        price: 12.45
+    },
+    {
+        title: "Third product",
+        price: 2.5
+    }];
+}
